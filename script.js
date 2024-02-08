0e9bc29b9edffa8210c372ecdba3e3518481f477
itsshawondevs <shawonuser24@gmail.com>
2024-02-08 22:45:55 +0600

    else if add

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ let date = document.querySelector('#date_time')
 let bloodName = document.querySelector('#blood_name')
 
 let mobileRegex = '^(?:\\+88|88)?(01[3-9]\\d{8})$'
-let emailRegex = '/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/'
+let emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
 const count = 5
 const numbers = '0123456789'
@@ -47,66 +47,111 @@ function random() {
     idNumber.innerHTML = mainId
 }
 
-
-
-createBtn.addEventListener('click',function(){
-    if(isNaN(inputName.value)){
-        nameErr.innerHTML = ''
-        studentName.innerHTML = inputName.value
-    }else{
+createBtn.addEventListener('click', function(){
+    if(!isNaN(inputName.value)){
         nameErr.innerHTML = '*please enter name here'
-        studentInfo.style.display = 'block'
     }
-
-    if(moNumber.value.match(mobileRegex)){
-        mobileNoErr.innerHTML = ''
-        mobileNumber.innerHTML = moNumber.value
-    }else{
+    
+    else if(!moNumber.value.match(mobileRegex)){
         mobileNoErr.innerHTML = '*please enter mobile number'
-        studentInfo.style.display = 'block'
     }
-
-    if(email.value.match(emailRegex)){
-        emailErr.innerHTML = ''
-    }else{
+    
+    else if(!email.value.match(emailRegex)){
         emailErr.innerHTML = '*please enter email'
     }
-
-    if(inputProgram.options.selectedIndex == 0){
+    
+    else if(inputProgram.options.selectedIndex == 0){
         programErr.innerHTML = '*plase select program'
-    }else{
+    }
+    
+    else if(selection.options.selectedIndex == 0){
+        bloodErr.innerHTML = '*plase select blood group'
+    }
+    
+    else if(startDate.value == ''){
+        dateErr.innerHTML = '*please enter starting date'
+    }
+    
+    else if(imgFile.value == ''){
+        imgErr.innerHTML = '*please select image'
+    }
+    
+    else{
+        nameErr.innerHTML = ''
+        studentName.innerHTML = inputName.value
+        mobileNoErr.innerHTML = ''
+        mobileNumber.innerHTML = moNumber.value
+        emailErr.innerHTML = ''
         programErr.innerHTML = ''
         programName.innerHTML = inputProgram.value
-    }
-    
-    if(selection.options.selectedIndex == 0){
-        bloodErr.innerHTML = '*plase select blood group'
-    }else{
         bloodErr.innerHTML = ''
         bloodName.innerHTML = selection.value
-    }
-
-    if(startDate.value == ''){
-        dateErr.innerHTML = '*please enter starting date'
-    }else{
         dateErr.innerHTML = ''
         date.innerHTML = startDate.value
-    }
-    
-    if(imgFile.value == ''){
-        imgErr.innerHTML = '*please select image'
-    }else{
         imgErr.innerHTML = ''
         imgFile = image.src = URL.createObjectURL(imgFile.files[0])
+        studentInfo.style.display = 'none'
+        studentId.style.display = 'block'
+        random()
     }
-        
-    random()
-
-    // studentInfo.style.display = 'none'
-    // studentId.style.display = 'block'
-
-
 })
+
+// createBtn.addEventListener('click',function(){
+//     if(isNaN(inputName.value)){
+//         nameErr.innerHTML = ''
+//         studentName.innerHTML = inputName.value
+//     }else{
+//         nameErr.innerHTML = '*please enter name here'
+//     }
+//     if(moNumber.value.match(mobileRegex)){
+//         mobileNoErr.innerHTML = ''
+//         mobileNumber.innerHTML = moNumber.value
+//     }else{
+//         mobileNoErr.innerHTML = '*please enter mobile number'
+//     }
+
+//     if(email.value.match(emailRegex)){
+//         emailErr.innerHTML = ''
+//     }else{
+//         emailErr.innerHTML = '*please enter email'
+//     }
+
+//     if(inputProgram.options.selectedIndex == 0){
+//         programErr.innerHTML = '*plase select program'
+//     }else{
+//         programErr.innerHTML = ''
+//         programName.innerHTML = inputProgram.value
+//     }
+    
+//     if(selection.options.selectedIndex == 0){
+//         bloodErr.innerHTML = '*plase select blood group'
+//     }else{
+//         bloodErr.innerHTML = ''
+//         bloodName.innerHTML = selection.value
+//     }
+
+//     if(startDate.value == ''){
+//         dateErr.innerHTML = '*please enter starting date'
+//     }else{
+//         dateErr.innerHTML = ''
+//         date.innerHTML = startDate.value
+//     }
+    
+//     if(imgFile.value == ''){
+//         imgErr.innerHTML = '*please select image'
+//     }else{
+//         imgErr.innerHTML = ''
+//         imgFile = image.src = URL.createObjectURL(imgFile.files[0])
+//     }
+        
+//     random()
+
+//     studentInfo.style.display = 'none'
+//     studentId.style.display = 'block'
+
+// })
+
+
 
 
 
